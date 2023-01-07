62c073b7e24d02b4c332bb76299a5f8ed234acb1
kingali371 <115498078+kingali371@users.noreply.github.com>
2023-01-07 13:51:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID" ,""))
+API_HASH = getenv("API_HASH" ,"")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("BOT_TOKEN" ,"")
+OWNER_ID = int(getenv("OWNER_ID" ,""))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI" ,"")
 MUST_JOIN = getenv("MUST_JOIN", None)
